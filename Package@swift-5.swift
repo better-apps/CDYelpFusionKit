e2b943d3e784ec9104f72bd05029c1a4f9b4bf10
better-apps <52092555+better-apps@users.noreply.github.com>
2020-03-18 15:12:03 -0700

    Update Package@swift-5.swift

--- Package@swift-5.swift
+++ Package@swift-5.swift
@@ -30,6 +30,10 @@ import PackageDescription
 
 let package = Package(
     name: "CDYelpFusionKit",
+    platforms: [.macOS(.v10_12),
+                .iOS(.v10),
+                .tvOS(.v10),
+                .watchOS(.v3)],
     products: [
         .library(
             name: "CDYelpFusionKit",
